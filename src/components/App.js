0ae7892bc29fb50544e84e88a96a191b80183218
Giovanni Pires da Silva <gio@bombin8.com.br>
2016-06-27 20:38:06 -0300

    Criando um contador em React.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import Contador from './Contador';
+
 export default class App extends React.Component {
 	render() {
-		return <h1>Olá React!</h1>;
+		return <Contador />;
 	}
 }
