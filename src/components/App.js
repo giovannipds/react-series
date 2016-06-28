08776a83b8c8bd3442f3562863163e1660e2adc0
Giovanni Pires da Silva <gio@bombin8.com.br>
2016-06-28 17:47:04 -0300

    Cria um placar estático, em React.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Contador from './Contador';
+import PlacarContainer from './PlacarContainer.js';
 
 export default class App extends React.Component {
 	render() {
-		return <Contador />;
+		return (
+			<div>
+				<PlacarContainer />
+				<Contador />
+			</div>
+		);
 	}
 }
